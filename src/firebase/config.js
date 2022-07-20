26d431fca5d898eae5cac5dfb6e2baa09fe10ff1
Tyler Mayfield <tyler.mayfield23@gmail.com>
2022-07-19 21:38:58 -0700

    started firebase initialization

---
+++ src/firebase/config.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDeqr_Ivl0f1aB6kZhcJcCHN9XRvuuzJAM",
+    authDomain: "finance-tracker-2eb80.firebaseapp.com",
+    projectId: "finance-tracker-2eb80",
+    storageBucket: "finance-tracker-2eb80.appspot.com",
+    messagingSenderId: "1060313966924",
+    appId: "1:1060313966924:web:e378e127a6ca7f019947e9"
+  };
+
+  //init firebase
+  firebase.initializeApp(firebaseConfig);
+
+  //init services
+  const projectFirestore = firebase.firestore;
+
+  export { projectFirestore }
